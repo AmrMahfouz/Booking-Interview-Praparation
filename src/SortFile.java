980cc37663a4cb1b9c22d33178995e91f656afcd
Amr Mahfouz <eng.csed.amr.mahfouz@gmail.com>
2018-11-09 12:21:28 +0200

    Initial commit

---
+++ src/SortFile.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class SortFile {
+
+    public static void main(String[] args) {
+        List<String> lines = Arrays.asList(
+                "",
+                "",
+                "",
+                ""
+        );
+    }
+
+    public void sortFile(List<String> lines) {
+
+    }
+
+}
